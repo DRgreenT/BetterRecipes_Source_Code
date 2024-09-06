65c7da3248d44c6f53371fb5050a17e9734e3f37
Thomas Just <thomas_just@outlook.com>
2024-09-06 19:20:04 +0300

    #CoI App link included #run_build.bat needs now enviroment variables

--- run_build.bat
+++ run_build.bat
@@ -1,10 +1,11 @@
 @echo off
-setlocal
 
-set "targetFolder=C:\Users\thoma\AppData\Roaming\Captain of Industry\Mods\BetterRecipes"
-set "sourceFile=G:\Programming IDEs\repos\CoI BetterRecipes Mod\bin\Debug\net472\publish\BetterRecipes.dll"
-set "projectFilePath=G:\Programming IDEs\repos\CoI BetterRecipes Mod\BetterRecipes.csproj"
-set "startGamePath=C:\Users\thoma\Desktop\Captain of Industry.url"
+set "modName=BetterRecipes"
+set "targetFolder=%COI_MOD_ROOT%%modName%\"
+set "sourceFile=%REPOS_ROOT%CoI %modName% Mod\bin\Debug\net472\publish\%modName%.dll"
+set "projectFilePath=%REPOS_ROOT%CoI %modName% Mod\%modName%.csproj"
+set "startGamePath=%REPOS_ROOT%CoI %modName% Mod\Captain of Industry.url"
+
 
 if not exist "%targetFolder%" (
     mkdir "%targetFolder%"
@@ -13,13 +14,12 @@ if not exist "%targetFolder%" (
 :loop
 
 cls
-
+echo "%COI_MOD_ROOT%
 dotnet publish "%projectFilePath%" /p:LangVersion=10.0
 if %errorlevel% equ 0 (
     copy /y "%sourceFile%" "%targetFolder%"
     if %errorlevel% equ 0 (
         echo Datei erfolgreich kopiert.
-        rem Hier kannst du eine Aktion ausführen, z.B. eine Datei öffnen
         start "" "%startGamePath%"
     ) else (
         echo Kopieren fehlgeschlagen.
